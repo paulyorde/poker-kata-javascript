1fda8d0c6afe423783a0184f95cc25407d4e582e
Paul Yorde <paulyorde26@gmail.com>
2017-05-25 13:59:28 -0400

    highest card completed

--- spec/HighestCardSpec.js
+++ spec/HighestCardSpec.js
@@ -1,32 +1,58 @@
 describe("HighestCard", function() {
-  describe("should return highest card of playerOne", function() {
-  	var playerOneHand = {"A":13};
-    var playerTwoHand = {"K":12};
+  describe("if player has highest card", function() {
+  	var playerOneHand = {
+      'suit': ['C', 'D', 'H', 'S'],
+      'face': [12,12,10,9,8]
+    };
+    var playerTwoHand = {
+      'suit': ['C', 'D', 'H', 'S'],
+      'face': [13,5,10,9,7]
+    };
     
-    it("Highest Card", function() {
-    	
+    it("should return highest card of player 2", function() {    	
       var underTest = new HighestCard();
 
       var result = underTest.determineFor(playerOneHand, playerTwoHand);
 
       expect(result).toBe(13);
     });
-
-    
   });
 
-  describe("should return tie if both hands have the same value", function() {
-  	var playerOneHand = {"A":13};
-    var playerTwoHand = {"A":13};
-    
-   
-    it("returns tie", function() {
-    	
+  describe("if players have the same value", function() {
+  	var playerOneHand = {
+      'suit': ['C', 'D', 'H', 'S'],
+      'face': [13,12,10,9,8]
+    };
+    var playerTwoHand = {
+      'suit': ['C', 'D', 'H', 'S'],
+      'face': [13,12,10,9,8]
+    };
+
+    it("should display tie", function() {   	
       var underTest = new HighestCard();
 
       var result = underTest.determineFor(playerOneHand, playerTwoHand);
 
       expect(result).toEqual("tie");
+    });
+  });
+
+  describe("if some cards match in both hands", function() {
+  	var playerOneHand = {
+      'suit': ['C', 'D', 'H', 'S'],
+      'face': [13,12,10,9,8]
+    };
+    var playerTwoHand = {
+      'suit': ['C', 'D', 'H', 'S'],
+      'face': [13,12,10,9,7]
+    };
+
+    it("should return highest card of player one", function() {   	
+      var underTest = new HighestCard();
+
+      var result = underTest.determineFor(playerOneHand, playerTwoHand);
+
+      expect(result).toEqual(8);
     });
   });
  
--- src/HighestCard.js
+++ src/HighestCard.js
@@ -1,26 +1,17 @@
 function HighestCard() {}
 
 HighestCard.prototype.determineFor = function(playerOneHand, playerTwoHand) {
-	var playerOneHighestCard = 0, playerTwoHighestCard = 0;
-	//var playerOneHasHighestCard = false, playerTwoHasHighestCard = false;
-
 	
+	var p1 = playerOneHand.face.sort(function(a, b){return b-a});
+	var p2 = playerTwoHand.face.sort(function(a, b){return b-a});
 
-	for(card in playerOneHand) {
-		playerOneHighestCard = playerOneHand[card];
-		
-	}
-
-	for(card in playerTwoHand) {
-		playerTwoHighestCard = playerTwoHand[card];
-		
-	}
-
-	if(playerOneHighestCard > playerTwoHighestCard)	{
-		return playerOneHighestCard;
-	}
-	else if(playerOneHighestCard < playerTwoHighestCard) {
-		return playerTwoHighestCard;
+	for(var card = 0; card < 5; card++) {
+		if(p1[card] > p2[card]) {
+			return p1[card];
+		}
+		else if(p1[card] < p2[card]) {
+			return p2[card];
+		}
 	}
 
 	return "tie"; // if both hands have same value - display tie;
